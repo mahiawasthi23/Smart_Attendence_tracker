c16c19280dc05b7b340c052b0b6330336c68ef0a
mahiawasthi23 <mahima23@navgurukul.org>
2025-10-07 15:45:07 +0530

    work on navbar code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import './App.css'
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 
 function App() {
-
-
   return (
-    <>
-      <div>
-        <h1>Attendence Tracker</h1>
-      </div>
-    </>
-  )
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/login" element={<h1>Login Page</h1>} />
+        <Route path="/signup" element={<h1>Signup Page</h1>} />
+        <Route path="/dashboard" element={<h1>Dashboard Page</h1>} />
+        <Route path="/profile" element={<h1>Profile Page</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
