71ec033ad5ea3fad4dc8eeceb7c4edd22620a7e0
nasrinakhatun23 <nasrinakhatun23@navgurukul.org>
2025-10-08 15:14:04 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,36 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./Pages/Login";
 import Sign from "./Pages/Sign";
+import Login from "./Pages/Login";
+import Navbar from "./Components/Navbar";
+
+import "./App.css";
+import "./Components/Navbar.css";
+import "./Pages/Sign.css";
+import "./index.css";
 
 function App() {
   const user = localStorage.getItem("user");
 
-
   return (
     <Router>
+      <Navbar />
       <Routes>
+       
         <Route
           path="/"
           element={
             user ? <Navigate to="/login" replace /> : <Navigate to="/signup" replace />
           }
         />
-        <Route path="/login" element={<Login />} />
+
         <Route path="/signup" element={<Sign />} />
-        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route path="*" element={<Navigate to="/signup" replace />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
